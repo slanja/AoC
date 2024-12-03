2bec1ba456eb031384115666410174bfaa847bb7
slanja <97552526+slanja@users.noreply.github.com>
2024-12-03 07:05:24 +0100

    commented the code

--- 2024/Day03/Main.py
+++ 2024/Day03/Main.py
@@ -13,11 +13,13 @@ with open("input.txt", "r") as f:
 
 pattern = r"mul\((\d+),(\d+)\)"
 
+# finding all parts that matches pattern
 for line in input:
     matches = re.findall(pattern, line)
 
 result = 0
 
+# multiplication of every set numbers, that was found
 for i in range(len(matches)):
     x, y = int(matches[i][0]), int(matches[i][1])
     result += mul(x, y)
